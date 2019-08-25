1540fc52baf09fb51108f1055b53a0e0ecca18e3
gbe <geoffrey.berard@lesfurets.com>
2019-08-25 21:21:21 +0200

    Allowing zoom

--- app/component/CardComponent.tsx
+++ app/component/CardComponent.tsx
@@ -6,11 +6,17 @@ interface CardComponentProps {
     card: Card;
 }
 
-export const CardComponent: React.FunctionComponent<CardComponentProps> = ({card}) => (
-    <div className={"CardComponent"}>
-        <img className={"CardComponent-img"}
-             alt={card.id}
-             title={card.label}
-             src={`http://www.magiccorporation.com/mc.php?rub=cartes&file=ajax&op=scan&num=${card.id}`}/>
-    </div>
-);
+export const CardComponent: React.FunctionComponent<CardComponentProps> = ({card}) => {
+    const [highlighted, setHighlighted] = React.useState(false);
+
+    return (
+        <div className={`CardComponent ${highlighted ? "highlighted" : ""}`}>
+            <img className={"CardComponent-img"}
+                 alt={card.id}
+                 title={card.label}
+                 onClick={() => setHighlighted(!highlighted)}
+                 onMouseLeave={() => setHighlighted(false)}
+                 src={`http://www.magiccorporation.com/mc.php?rub=cartes&file=ajax&op=scan&num=${card.id}`}/>
+        </div>
+    );
+}
